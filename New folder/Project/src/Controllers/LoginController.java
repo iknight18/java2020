85eb7293a18ecf7bd6e2c3993adb5aeadaafdb0c
iknight18 <feresnefzi18@gmail.com>
2020-12-06 21:52:59 +0100

    added to ignore

--- New folder/Project/src/Controllers/LoginController.java
+++ New folder/Project/src/Controllers/LoginController.java
@@ -6,15 +6,12 @@
 package Controllers;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+
 
 /**
  * FXML Controller class
